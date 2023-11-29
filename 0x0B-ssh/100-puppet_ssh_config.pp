db8b791af162c7331e5364932318be21ea42d19a
Martin-do <martinjaiyeola40@gmail.com>
2023-11-29 14:11:40 -0800

    "created puppet manifest file to alter ssh config"

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# puppet to make changes to config file
+
+include stdlib
+
+file_line { 'Refuse to authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
