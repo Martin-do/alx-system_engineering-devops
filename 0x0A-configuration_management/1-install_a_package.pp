31342f60c38ce893bc1419722efe96433a0ccc24
Martin-do <martinjaiyeola40@gmail.com>
2023-11-29 16:32:25 -0800

    "refactored"

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
 # Using Puppet, install flask from pip3.
-package { 'python3-pip':
-  ensure  => '2.1.0',
-}
-
-exec { 'install Flask':
-  command => 'pip install Flask',
-  path    => ['/usr/bin'],
+package { 'flask':
+  ensure   => '2.1.1',
+  provider => 'pip'
 }
